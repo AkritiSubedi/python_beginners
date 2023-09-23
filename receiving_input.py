37b1fb26ea8e3ec417067f96dbe08c6c35155898
AkritiSubedi <aakusubedi075@gmail.com>
2023-09-23 13:42:43 +0545

    Receiving input

---
+++ receiving_input.py
@@ -0,0 +1,5 @@
+#Ask two questions: person's name favourite color and print message like "Akriti likes blue
+person_name = input("What is your name ?")
+favourite_color = input("What is your favourite color ?")
+print(person_name + ' Likes  ' + favourite_color)
+
