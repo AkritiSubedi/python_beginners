794fbf98bf026f5a556ef8dddfe8abfc07b0c059
AkritiSubedi <aakusubedi075@gmail.com>
2023-09-24 13:23:57 +0545

    type_conversion

---
+++ type_conversion.py
@@ -0,0 +1,8 @@
+birth_year = input('Birth year:')
+age = 2023 - int(birth_year)
+
+print(age)
+# ask user weight in pounds and convert it to kilograms and print on the terminal
+weight_lbs = input('Weight (lbs): ')
+weight_kg = int(weight_lbs) * 0.45
+print(weight_kg)
