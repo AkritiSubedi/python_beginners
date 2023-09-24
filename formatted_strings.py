dde98536a595c17076c8a6d247c09f83f09af06e
AkritiSubedi <aakusubedi075@gmail.com>
2023-09-24 13:42:53 +0545

    formatted_strings

---
+++ formatted_strings.py
@@ -0,0 +1,7 @@
+# to print Akriti [Subedi] is a learner
+first_name = 'Akriti'
+last_name = 'Subedi'
+message = first_name + ' [' + last_name + '] is a learner'
+msg = f'{first_name} [{last_name}] is a learner' # formatted string
+print(message)
+print(msg)
