24d81b7bcba3bd04641bece471db9b909a752b96
AkritiSubedi <aakusubedi075@gmail.com>
2023-09-25 15:46:55 +0545

    python_beginners

--- If_statements.py
+++ If_statements.py
@@ -9,3 +9,10 @@ elif is_cold:
 else:
     print("Its a lovely day")
 
+price = 1000000
+has_good_credit = True
+if has_good_credit:
+    down_payment = 0.1 * price
+else:
+    down_payment = 0.2 * price
+print(f"Down payment: ${down_payment}")
