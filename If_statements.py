750710bf1eace6ac935cfbc1d456a81df2592cca
AkritiSubedi <aakusubedi075@gmail.com>
2023-09-25 15:41:06 +0545

    python_beginners

---
+++ If_statements.py
@@ -0,0 +1,11 @@
+is_hot = False
+is_cold = False
+if is_hot:
+    print("Its a hot day")
+    print("Enjoy your day")
+elif is_cold:
+    print("its a cold day")
+    print(("Wear warm cloth"))
+else:
+    print("Its a lovely day")
+
