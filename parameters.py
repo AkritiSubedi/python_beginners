1c93bf1e6aaa84845d133e07e1fb226cc05b25e6
AkritiSubedi <aakusubedi075@gmail.com>
2023-10-01 15:57:54 +0545

    parameters

---
+++ parameters.py
@@ -0,0 +1,9 @@
+def greet_user(first_name, Last_name):
+    print(f'Hi {first_name} {Last_name}')
+    print('Welcome here')
+
+
+print("Start")
+greet_user("Akriti" , "Subedi")
+greet_user("Kriti", "Singh")
+print("Finish")
