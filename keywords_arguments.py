b02e4da069d25c70496ba95d89685de84e98f408
AkritiSubedi <aakusubedi075@gmail.com>
2023-10-01 15:57:38 +0545

    keywords_arguments

---
+++ keywords_arguments.py
@@ -0,0 +1,8 @@
+def greet_user(first_name, Last_name):
+    print(f'Hi {first_name} {Last_name}')
+    print('Welcome here')
+
+
+print("Start")
+greet_user(Last_name="Akriti" , first_name= "Subedi")
+print("Finish")
