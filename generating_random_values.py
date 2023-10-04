8451e52bf30e20de76b7a7334f6ee057b6781331
AkritiSubedi <aakusubedi075@gmail.com>
2023-10-04 16:36:26 +0545

    generating random values

---
+++ generating_random_values.py
@@ -0,0 +1,10 @@
+import random
+
+for i in range(3):
+    #print(random.random())
+    print(random.randint(10, 20))
+members = ['Akriti', 'Prakriti', 'Kriti', 'Riti']
+leader = random.choice(members)
+print(leader)
+
+
